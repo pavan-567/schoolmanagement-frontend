f6f3216c9ddd9e6d23101ff2be0c77a606627c33
Pavan Ram Gangadhar Kumar <gangathecoder425@gmail.com>
2023-11-30 10:40:12 +0530

    Fixed The Functionality Of Deleting a Student + Some Minor Improvements

--- src/app/stu-delete/stu-delete.component.ts
+++ src/app/stu-delete/stu-delete.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { StudentService } from '../services/Student.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Student } from '../models/Student.model';
+import { StudentFee } from '../models/StudentFee.model';
 
 @Component({
   selector: 'app-stu-delete',
@@ -14,20 +15,21 @@ export class StuDeleteComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router
   ) {}
-  stuData: Student = null;
+
+  stuFee: StudentFee = null;
   isFetched: boolean = false;
 
   ngOnInit(): void {
     let id = +this.route.snapshot.params['id'];
-    this.stu.fetchStudentWithFee(id).subscribe((res) => {
-      this.stuData = res;
-      console.log(this.stuData);
+    this.stu.fetchStuFeeData(id).subscribe((res) => {
+      this.stuFee = res;
+      console.log(this.stuFee);
       this.isFetched = true;
     });
   }
 
   onDeleteStu() {
-    this.stu.deleteStudentWithId(+this.stuData.id).subscribe((res) => {
+    this.stu.deleteStudentWithId(+this.stuFee.id).subscribe((res) => {
       console.log(res);
     });
     this.router.navigate(['/students', 'display']);
